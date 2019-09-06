c8b281a7fbbf28541ba89bb1f5503eb0aafb7ca4
recurze <vrahul24999@gmail.com>
2019-09-06 12:15:14 +0530

    Remove time and error computation; not the job of main

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,7 @@
 #include <iostream>
 
 void in(const char *fname, Graph *g, double *b);
-void out(const char *fname, int n, double *x, double e, double t);
-double computeError(int n, double **L, const double *x, const double *b);
+void out(const char *fname, int n, double *x);
 
 int main(int argc, char **argv) {
     if (argc != 3) {
@@ -21,25 +20,18 @@ int main(int argc, char **argv) {
     char *ifname = argv[0];
     in(ifname, g, b);
 
-    auto ts = std::chrono::high_resolution_clock::now();
     double *x = NULL;
     Lsolver().solve(g, b, x);
-    auto te = std::chrono::high_resolution_clock::now();
 
+    int n = g->getNumVertex();
 
-    auto n = g->getNumVertex();
-
-    double **L = new double*[n];
-    for (int i = 0; i < n; ++i) {
-        L[i] = new double[n];
-    }
-    g->copyLaplacianMatrix(L);
-    auto error = computeError(n, L, x, b);
-
-    auto time = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();
+    delete g; g = NULL;
+    delete[] b; b = NULL;
 
     char *ofname = argv[1];
-    out(ofname, n, x, error, time);
+    out(ofname, n, x);
+
+    delete[] x; x = NULL;
 
     return 0;
 }
@@ -47,43 +39,27 @@ int main(int argc, char **argv) {
 void in(const char *fname, Graph *g, double *b) {
     std::ifstream infile(fname);
 
-    int numVertex;
-    infile >> numVertex;
+    int n;
+    infile >> n;
 
-    b = new double[numVertex];
-
-    double **A = new double*[numVertex];
-    for (int i = 0; i < numVertex; ++i) {
-        A[i] = new double[numVertex];
-        for (int j = 0; j < numVertex; ++j) {
+    double **A = new double*[n];
+    for (int i = 0; i < n; ++i) {
+        A[i] = new double[n];
+        for (int j = 0; j < n; ++j) {
             infile >> A[i][j];
         }
+    }
+    g = new Graph(n, A);
+
+    b = new double[n];
+    for (int i = 0; i < n; ++i) {
         infile >> b[i];
     }
-
-    g = new Graph(numVertex, A);
 }
 
-double computeError(int n, const double **L, const double *x, const double *b) {
-    double mse = 0;
-    for (int i = 0; i < n; ++i) {
-        double e = -b[i];
-        for (int j = 0; j < n; ++j) {
-            e += L[i][j] * x[j];
-        }
-        mse += e*e;
-    }
-    mse /= n;
-    return sqrt(mse);
-}
-
-void out(const char *fname, int n, double *x, double e, double t) {
+void out(const char *fname, int n, double *x) {
     std::ofstream outfile(fname);
-    outfile << "Error: " << e << '\n';
-    outfile << "Time taken: " << t << '\n';
-
     for (int i = 0; i < n; ++i) {
         outfile << x[i] << (i == n - 1 ? '\n' : ' ');
     }
 }
-
