7caeba039176935c60658f0f979574992fa218fe
recurze <vrahul24999@gmail.com>
2019-09-11 23:31:45 +0530

    Move checker functions to new file

--- testgen.py
+++ testgen.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
 
 '''
-Generates 1 files
-    n.inp
-    where n is the size of the graph, fed as command line argument
+Purpose:
+    Generate undirected positive weighted connected graph of given size
 
-    n.inp contains the graph and b according to the format specified in README
+Command line argument:
+    n: integer denoting number of nodes in the graph
+
+Console Output:
+    If not run properly, instructions on usage; else none
+
+File output:
+    filename: n.inp; replace n with integer
+    Contents: n + 2 lines; format as specified in the README
 '''
 
 import sys
 import random
 import numpy as np
 
-minw, maxw = 0.1, 100.0
+minw, maxw = 1, 1000
 def generate_undirectedWeightedConnectedGraph(n, m):
     assert m >= n - 1 and m <= n*(n - 1)/2
-    msg = "Generating a random undirected weighted connected graph with {} vertices and < {} edges".format(n, m + 1)
-    print(msg)
 
     nodes = list(range(0, n))
     A = [[0 for i in range(n)] for j in range(n)]
@@ -48,76 +53,31 @@ def generate_undirectedWeightedConnectedGraph(n, m):
     add_random_edges(m - n + 1)
     return A
 
-minb, maxb = 0, 100
+minb, maxb = 0, 100000
 def generate_randomb(n):
     b = [random.uniform(minb, maxb) for _ in range(n - 1)]
     b.append(-sum(b))
     return b
 
-def getL(A):
-    D = [sum(row) for row in A]
-    n = len(A)
-    L = []
-    for i in range(n):
-        l = [-x for x in A[i]]
-        l[i] = D[i]
-        L.append(l)
-    return L
-
-
-def solve(A, b):
-    L = np.array(getL(A))
-    print(L)
-    print(np.linalg.det(L))
-    b = np.array(b)
-    x = np.linalg.solve(L, b)
-    print(np.sum(x))
-    return x
-
-def readIn(ifname):
-    with open(ifname) as f:
-        content = f.readlines()
-    content = [x.strip() for x in content]
-    n = int(content[0])
-    A = []
-    for i in range(n):
-        A.append([float(x) for x in content[i + 1].split()])
-    b = [float(x) for x in content[n + 1].split()]
-    return getL(A), b
-
-def readOut(ofname):
-    with open(ofname) as f:
-        content = f.readlines()
-    content = [x.strip() for x in content]
-    return [float(i) for i in content[0].split()]
+def writeToFile(n, A, b):
+    ifname = str(n) + '.inp'
+    with open(ifname, 'w') as f:
+        print(n, file=f)
+        for i in range(n):
+            for j in range(n):
+                print(A[i][j], end = " " if j < n - 1 else '', file = f)
+            print("", file = f)
+        for i in range(n):
+            print(b[i], end = " " if i < n - 1 else '', file = f)
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        n = int(sys.argv[1])
-        m = random.randint(n - 1, n*(n - 1)/2)
-        A = generate_undirectedWeightedConnectedGraph(n, m)
-        b = generate_randomb(n)
+    if len(sys.argv) != 2:
+        print("Example Usage: ./testgen.py 10")
+        exit(0)
+    n = int(sys.argv[1])
+    m = random.randint(n - 1, n*(n - 1)/2)
+    A = generate_undirectedWeightedConnectedGraph(n, m)
+    b = generate_randomb(n)
 
-        ifname = str(n) + '.inp'
-        with open(ifname, 'w') as f:
-            print(n, file=f)
-            for i in range(n):
-                for j in range(n):
-                    print(A[i][j], end = " " if j < n - 1 else '', file = f)
-                print("", file = f)
-            for i in range(n):
-                print(b[i], end = " " if i < n - 1 else '', file = f)
-        print(solve(A, b))
-    if len(sys.argv) == 3:
-        ifname = sys.argv[1]
-        ofname = sys.argv[2]
-        L, b = readIn(ifname)
-        x = readOut(ofname)
-
-        L = np.array(L)
-        x = np.array(x)
-        b = np.array(b)
-        print(b)
-        print(np.dot(L, x))
-        print(np.linalg.norm(np.dot(L, x)-b))
+    writeToFile(n, A, b)
 
